6259ade862567255740475099a9f33ce08bffc52
Nawwal Mohammed <102904919+Nawwalicious@users.noreply.github.com>
2024-04-02 16:57:16 +0530

    Test Change
    Test Change

--- test.js
+++ test.js
@@ -1,9 +1,21 @@
 var maxFrequencyElements = function(nums) {
     let res=[]
+    nums.sort((a,b)=>{return a-b})
     for(let i=0;i<nums.length;i++)
     {
-        
+        for(let j=i;j<nums.length;j++)
+        {
+            if(i!=j && nums[i]==nums[j])
+            {
+                res.push(nums[i],nums[j])
+                res.splice(i,1)
+                res.splice(j,1)
+            }
+        }
     }
+    return res.length>0?res.length:nums.length;
 };
-nums=[1,2,2,3,1,4];
+
+let nums=[19,19,19,20,19,8,19];
+
 console.log(maxFrequencyElements(nums))
